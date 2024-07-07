e9d59b8c38fc071be4a1974de1d89bb08eab1d43
Haridharshini21500176 <vsvel79@gmail.com>
2024-07-07 13:21:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import GetData from './Components/getData'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <GetData></GetData>
+    </React.Fragment>
+  )
+}
+
+export default App
